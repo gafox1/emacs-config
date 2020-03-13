31d6f2eed2ad5ac19365c26f94dcffe515c6095d
Gordon Fox <gfox@unm.edu>
2020-03-12 22:10:23 -0600

    1st commit w .emacs.d

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -1,1 +1,26 @@
-c:/users/gafox/.emacs-custom.el
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be
+careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success
+warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue"
+"magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-enabled-themes (quote (adwaita)))
+ '(desktop-save-mode t)
+ '(doc-view-continuous t)
+ '(latex-run-command "pdflatex")
+ '(package-selected-packages (quote (gited org)))
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be
+careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Courier New" :foundry "outline"
+:slant normal :weight bold :height 142 :width normal)))))
